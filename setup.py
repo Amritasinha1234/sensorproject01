3b8b00a5bfaf0986a2618c3a486d5ede096effd6
Amritasinha1234 <amrita.sinha23-25@bibs.co.in>
2024-12-28 11:16:06 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)-> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    return requirements    
+
+setup(  
+    name="Fault detection",
+    version="0.0.1",
+    author="Amrita",
+    author_email="amrita.sinha23-25@bibs.co.in",
+    install_requirements=get_requirements("requirements.txt"),
+    packages=find_packages()
+    
+    
+    
+)
+
